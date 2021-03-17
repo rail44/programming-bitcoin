f0418d30cee511ecdef07dc3d7a1bfb4f87a8f74
Satoshi Amemiya <satoshi_amemiya@voyagegroup.com>
2021-03-17 17:24:20 +0900

    2.5

--- src/point.rs
+++ src/point.rs
@@ -1,28 +1,61 @@
 use anyhow::{anyhow, Result};
+use std::ops;
 
 #[derive(Debug, PartialEq, Clone)]
-struct Point {
-    a: i64,
-    b: i64,
-    x: i64,
-    y: i64,
+enum Point {
+    Inf,
+    Normal {
+        x: i64,
+        y: i64,
+    }
 }
 
-impl Point {
-    fn new(x: i64, y: i64, a: i64, b: i64) -> Result<Point> {
+#[derive(Debug, PartialEq, Clone)]
+struct CurvePoint {
+    a: i64,
+    b: i64,
+    p: Point,
+}
+
+impl CurvePoint {
+    fn new(x: i64, y: i64, a: i64, b: i64) -> Result<CurvePoint> {
         if y.pow(2) != x.pow(3) + a * x + b {
-            return Err(anyhow!("Cannot add two numbers in deffirent Fields"));
+            return Err(anyhow!("({}, {}) is not on the curve", x, y));
         }
-        Ok(Point { a, b, x, y })
+        Ok(CurvePoint { a, b, p: Point::Normal { x, y } })
+    }
+
+    fn inf(a: i64, b: i64) -> CurvePoint {
+        CurvePoint { a, b, p: Point::Inf }
+    }
+}
+
+impl ops::Add<CurvePoint> for CurvePoint {
+    type Output = Result<CurvePoint>;
+
+    fn add(self, other: CurvePoint) -> Result<CurvePoint> {
+        if self.a != other.a || self.b != other.b {
+            return Err(anyhow!("Points {:?}, {:?} are not on the same curve", self, other));
+        }
+
+        if self.p == Point::Inf {
+            return Ok(other);
+        }
+
+        if other.p == Point::Inf {
+            return Ok(self);
+        }
+
+        panic!();
     }
 }
 
 
 #[test]
 fn test_new() {
-    let p1 = Point::new(-1, -1, 5, 7);
+    let p1 = CurvePoint::new(-1, -1, 5, 7);
     assert!(p1.is_ok());
 
-    let p2 = Point::new(-1, -2, 5, 7);
+    let p2 = CurvePoint::new(-1, -2, 5, 7);
     assert!(p2.is_err());
 }
