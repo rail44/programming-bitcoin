3737e7e219077fdf3f759b4ce876b1918a771687
Satoshi Amemiya <satoshi_amemiya@voyagegroup.com>
2021-05-26 18:05:09 +0900

    wip

--- src/s256.rs
+++ src/s256.rs
@@ -2,7 +2,7 @@ use crate::field_element::{FieldElement, Prime};
 use crate::point::{Curve, CurvePoint};
 use anyhow::Result;
 use num_bigint::{BigInt, Sign};
-use num_traits::Pow;
+use num_traits::{ToPrimitive, Pow};
 use once_cell::sync::Lazy;
 use rand::seq::IteratorRandom;
 use std::ops;
@@ -39,6 +39,29 @@ static G: Lazy<S256Point> = Lazy::new(|| {
     )
     .unwrap()
 });
+
+static BASE58_ALPHABET: &str = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+fn encode_base58(bytes: &[u8]) -> String {
+    let mut count = 0;
+    for b in bytes {
+        if b == &0 {
+            count += 1;
+            continue;
+        }
+        break;
+    }
+
+    let mut num = BigInt::from_bytes_be(Sign::Plus, bytes);
+    let prefix = "1".repeat(count);
+    let mut result = String::new();
+    while num > 0.into() {
+        let mod_num = (num.clone() % BigInt::from(58)).to_u8().unwrap();
+        num = num / 58;
+        result = format!("{}{}", BASE58_ALPHABET.chars().nth(mod_num.into()).unwrap(), result);
+    }
+    format!("{}{}", prefix, result)
+}
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct S256Field<'a> {
@@ -374,4 +397,17 @@ fn test_exam_4_3() {
     let der = sig.der();
     let hex = der.iter().map(|n| format!("{:02x}", n)).collect::<String>();
     assert_eq!(hex, "3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c60221008ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec".to_string());
+}
+
+#[test]
+fn test_exam_4_4() {
+    let b = BigInt::parse_bytes(
+        b"7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d",
+        16,
+    )
+    .unwrap().to_bytes_be().1;
+
+    let base58 = encode_base58(&b);
+
+    assert_eq!(base58, "9MA8fRQrT4u8Zj8ZRd6MAiiyaxb2Y1CMpvVkHQu5hVM6".to_string());
 }
